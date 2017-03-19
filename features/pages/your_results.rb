94b31323075c66e0cd1a5d96d2ee0f316c3b283b
Deepika Dixit <deepikadixit18@gmail.com>
2017-03-19 00:06:25 +0000

    pom results

--- features/pages/your_results.rb
+++ features/pages/your_results.rb
@@ -5,6 +5,15 @@ module CompareEnergy
       wait_for_results_heading(10)
     end
 
+    def get_current_tariff_heading
+      current_tariff_heading.text
+    end
+
+    def get_heading
+      results_heading.text
+    end
+
+    private
     element :current_tariff_heading, :xpath, "/html/body/div/div/main/section[1]/div/h2"
 
     element :results_heading, :xpath, "/html/body/div/div/main/section[3]/h2"
